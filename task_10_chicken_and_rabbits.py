099922e83c0e465486f3826d727e6c6972664eac
Alexandra Falk-Schmidtberger <nebulaleaf@gmail.com>
2024-06-08 21:00:48 +0200

    chicken and rabbits, addition possible legs

--- task_10_chicken_and_rabbits.py
+++ task_10_chicken_and_rabbits.py
@@ -6,19 +6,20 @@ Output of the program should tell us how many rabbits and how many chickens we h
 
 heads = 35
 legs = 94
-chicken = []
-rabbits = []
 
-for i in range(heads + 1):
-    j = heads - i
-    if 2 * i + 4 * j == legs:
-        chicken.append(i)
-        rabbits.append(j)
-
-if chicken and rabbits:  # If the lists are not empty, a solution was found
-    print(f"Number of chickens: {chicken[0]}")
-    print(f"Number of rabbits: {rabbits[0]}")
-else:  # If the lists are empty, no solution was found
+if legs % 2 != 0 or legs < 2 * heads or legs > 4 * heads:
     print("No solution found with the given number of heads and legs.")
+else:
+    chicken = []
+    rabbits = []
+    for i in range(heads + 1):
+        j = heads - i
+        if 2 * i + 4 * j == legs:
+            chicken.append(i)
+            rabbits.append(j)
 
-
+    if chicken and rabbits:
+        print(f"Number of chickens: {chicken[0]}")
+        print(f"Number of rabbits: {rabbits[0]}")
+    else:
+        print("No solution found with the given number of heads and legs.")
